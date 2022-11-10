5c5d1d88358b8c7031957c42611d2f242c8c22ae
Soren Umstot <soren.umstot@gmail.com>
2022-11-10 17:00:59 +0900

    started to add styling to the show page for the lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,35 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
+
+# As a user, I can see all my movie lists
+# As a user, I can create a movie list
+# As a user, I can see the details of a movie list
+# As a user, I can bookmark a movie inside a movie list
+# As a user, I can destroy a bookmark
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 require 'open-uri'
 Movie.destroy_all
 # List.destroy_all
-
+puts 'Seeding database'
 # the Le Wagon copy of the API
 url = 'http://tmdb.lewagon.com/movie/top_rated'
 response = JSON.parse(URI.open(url).read)
@@ -12,7 +12,10 @@ response['results'].each do |movie_hash|
   p movie_hash
 
   Movie.create!(
+    title: movie_hash['original_title'],
+    overview: movie_hash['overview'],
     poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
-    ...
   )
 end
+
+puts "Seeded DB with #{Movie.all.count} movies"
