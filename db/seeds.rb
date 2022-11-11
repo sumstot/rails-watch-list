80f2fb5b4d6428ef3c8593c726260dafd75e45bd
Soren Umstot <soren.umstot@gmail.com>
2022-11-11 15:01:33 +0900

    all tests passed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ response['results'].each do |movie_hash|
   Movie.create!(
     title: movie_hash['original_title'],
     overview: movie_hash['overview'],
-    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
+    poster_url: "https://image.tmdb.org/t/p/w500" + "#{movie_hash['poster_path']}"
   )
 end
 
